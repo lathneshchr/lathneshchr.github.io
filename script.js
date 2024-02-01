0bf6158201f2631a6fa1f9efbe7a7a839e9dd320
Lathnesh Cheruvattil <45116884+lathneshchr@users.noreply.github.com>
2024-02-01 23:18:40 +0530

    web start
    initiate

---
+++ script.js
@@ -0,0 +1,78 @@
+//javascript for navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+//javascript for responsive navigation sidebar menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation a")
+
+menuBtn.addEventListener("click",  () => {
+  menuBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+navigationItems.forEach((navigationItem) => {
+  navigationItem.addEventListener("click", () => {
+    menuBtn.classList.remove("active");
+    navigation.classList.remove("active");
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var terminalTitle = document.getElementById('terminal-title');
+  var text = 'Lathnesh';
+  var index = 0;
+
+  function type() {
+    terminalTitle.textContent += text[index];
+    index++;
+
+    if (index < text.length) {
+      setTimeout(type, 100);
+    }
+  }
+
+  type();
+});
+
+
+(function() {
+  var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+
+  // Your existing script.js code here
+  // ...
+})();
+
+
+//javascript for scroll to top button
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+  scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+//javascript for scroll back to top on click scroll-to-top button
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+//javascript for reveal website elements on scroll
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll(".reveal");
+
+  for(var i = 0; i < reveals.length; i++){
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 50;
+
+    if(revealTop < windowHeight - revealPoint){
+      reveals[i].classList.add("active");
+    }
+  }
+}
